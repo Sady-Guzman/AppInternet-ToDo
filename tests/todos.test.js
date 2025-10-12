62de9e3607609219da370fd2155670a0aa763df2
Sady Guzman <102682450+Sady-Guzman@users.noreply.github.com>
2025-10-12 18:21:55 -0300

    add jest a test todo

--- tests/todos.test.js
+++ tests/todos.test.js
@@ -1,6 +1,15 @@
+import { jest } from "@jest/globals";
+import handler from "../api/todos.js";
+
 test("Crear un ToDo sin user_id devuelve 400", async () => {
   const req = { method: "POST", body: { text: "Tarea de prueba" } };
-  const res = { status: jest.fn().mockReturnThis(), json: jest.fn(), setHeader: jest.fn(), end: jest.fn() };
+
+  const res = { 
+    status: jest.fn().mockReturnThis(), 
+    json: jest.fn(), 
+    setHeader: jest.fn(), 
+    end: jest.fn() 
+  };
 
   await handler(req, res);
 
